5c1abada611e8d2a7b74512eae089d5482aee425
user <user@pop-os.localdomain>
2023-03-27 23:40:22 -0400

    fix directory and traversal

--- md-to-html.sh
+++ md-to-html.sh
@@ -1,2 +1,4 @@
+mkdir html
 for i in *.md ; do echo "$i" && pandoc -s $i -t html -o html/$i.html --lua-filter=links-to-html.lua; done
+cd html
 for file in *.md.html ; do   mv "$file" "${file%.md.html}.html" ;done
