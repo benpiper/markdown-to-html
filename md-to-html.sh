a6bd895fb0882d66d8695aefab275fdc8f60885a
user <user@pop-os.localdomain>
2023-03-27 23:28:53 -0400

    test html titles

--- md-to-html.sh
+++ md-to-html.sh
@@ -1,2 +1,4 @@
-for i in *.md ; do echo "$i" && pandoc -s $i -t html -o html/$i.html --lua-filter=links-to-html.lua; done
+mkdir html
+for i in *.md ; do echo "$i" && pandoc -s $i -t html -o html/$i.html --lua-filter=links-to-html.lua --metadata title="$(head -1 $i)"; done
+cd html
 for file in *.md.html ; do   mv "$file" "${file%.md.html}.html" ;done
